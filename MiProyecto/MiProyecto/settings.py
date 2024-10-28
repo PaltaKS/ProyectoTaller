936c5d8e3781b6f500db4acdc0fa6ef3bac7e8f9
Bastian Pavez <bpavez03@gmail.com>
2024-10-28 05:04:27 -0300

    configuracion DB

--- MiProyecto/MiProyecto/settings.py
+++ MiProyecto/MiProyecto/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +74,14 @@ WSGI_APPLICATION = 'MiProyecto.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyecto2',
+        'USER': 'root',
+        'PASSWORD': 'root12345.',
+        'HOST': 'trabajotaller.cbqgkgiuafaz.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
